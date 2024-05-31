7660ffb56e0788f6ef747ca380231d7d2e7eef75
tonymk0987 <k.tony.muriuki@gmail.com>
2024-05-31 21:26:47 +0300

    Complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):#instantiates with a single argument, a word. .  
+        self.word = word.lower()
+
+    def match(self, word_list):#contains a method called "match". .
+        # Filters the word list to include only words that are anagrams of self.word
+        return [word for word in word_list if sorted(word.lower()) == sorted(self.word)]
